6e7ee0b85b1fc1ee359e8e3421f78c0fe023cb2e
Skiecik <skiecik@skietcode.hub.pl>
2018-06-11 22:07:39 +0200

    day1 arrays exercises

---
+++ src/day1/arrays/FilledArray.java
@@ -0,0 +1,13 @@
+package day1.arrays;
+
+import java.util.Arrays;
+
+public class FilledArray {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		Arrays.fill(arr, 2);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+}
---
+++ src/day1/arrays/ReverseArray.java
@@ -0,0 +1,22 @@
+package day1.arrays;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+	
+		int[] arr = RandomArray.randArrGenerator(10, 20);
+		int[] reverseArr = new int[10];
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		System.out.println();
+		System.out.println("---------------------");
+		
+		for (int i = 0; i < arr.length; i++) {
+			reverseArr[i] = arr[arr.length-1-i];
+			System.out.print(reverseArr[i]+" ");
+		}
+	}
+}
