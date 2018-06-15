a86dddbc78af1d6669834f0671f8d9092d28525b
Skiecik <skieck@skietcode.hub.pl>
2018-06-15 23:01:52 +0200

    finished input exercises

--- src/week1/day4/input/QuadEquation.java
+++ src/week1/day4/input/QuadEquation.java
@@ -15,6 +15,33 @@ public class QuadEquation {
 			System.out.println("Not a correct number. Try again");
 		}
 		a = scan.nextInt();
+		System.out.println(a);
+		System.out.println("Please type three factors of the equation.");
+		System.out.println("Please type factor \"b\":");
+		while (!scan.hasNextInt()) {
+			scan.nextLine();
+			System.out.println("Not a correct number. Try again");
+		}
+		b = scan.nextInt();
+		System.out.println(b);
+		System.out.println("Please type three factors of the equation.");
+		System.out.println("Please type factor \"c\":");
+		while (!scan.hasNextInt()) {
+			scan.nextLine();
+			System.out.println("Not a correct number. Try again");
+		}
+		c = scan.nextInt();
+		System.out.println(c);
 		
+		double delta = b*b - 4*a*c; 
+		double eq1 = (-b - Math.sqrt(delta))/(2*a);
+		double eq2 = (-b + Math.sqrt(delta))/(2*a);
+		
+		System.out.println("eq1 = " + eq1 + " eq2 = " + eq2);
+		scan.close();
+	}
+	
+	public static void main(String[] args) {
+		equation();
 	}
 }
