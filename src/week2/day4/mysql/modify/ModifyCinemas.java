6c3b73011f8284443a59d755643bf3e212f145ea
Skiecik <skieck@skietcode.hub.pl>
2018-06-21 13:12:07 +0200

    add one file

--- src/week2/day4/mysql/modify/ModifyCinemas.java
+++ src/week2/day4/mysql/modify/ModifyCinemas.java
@@ -2,6 +2,9 @@ package week2.day4.mysql.modify;
 
 public class ModifyCinemas {
 
-	private String 
+	private String query1 = "ALTER TABLE Movies ADD watchCount INT";
+	private String quetry2 = "ALTER TABLE Movies ADD isTop TINYINT";
+	private String quetry3 = "ALTER TABLE Cinemas ADD openTime TIME";
+	private String quetry4 = "ALTER TABLE Cinemas ADD closeTime TIME";
 	
 }
